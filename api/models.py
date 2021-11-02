007b15191bf81de5541e4a020990dadc95779e46
jomomg <jomomg@gmail.com>
2021-11-02 12:07:04 +0300

    create user, role and permission models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,34 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class Permission(models.Model):
+    """
+    Permissions model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+
+class Role(models.Model):
+    """
+    Roles model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    permissions = models.ManyToManyField(Permission)
+
+
+class User(AbstractBaseUser):
+    """
+    User model
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    other_names = models.CharField(max_length=150)
+    roles = models.ManyToManyField(Role)
